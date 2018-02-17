28fc5cd17c88242a37c7b925d75e0754eebac602
hacc <haccel@my.com>
2018-02-17 23:00:25 +0300

    works

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
   <header>
     <nav>
       <router-link to="/" class="button">Home</router-link>
-      <router-link to="dashboard" class="button" v-if="user">Dashboard</router-link>
-      <router-link to="sign-in" class="button" v-if="!user">Sign-in</router-link>
-      <router-link to="sign-up" class="button" v-if="!user">Sign-up</router-link>
-      <a class="button" v-on:click="signOut" v-if="user">Sign-out</a>
+      <router-link to="dashboard" class="button" v-if="user.signed">Dashboard</router-link>
+      <router-link to="sign-in" class="button" v-if="!user.signed">Sign-in</router-link>
+      <router-link to="sign-up" class="button" v-if="!user.signed">Sign-up</router-link>
+      <a class="button" v-on:click="signOut" v-if="user.signed">Sign-out</a>
     </nav>
   </header>
 </template>
